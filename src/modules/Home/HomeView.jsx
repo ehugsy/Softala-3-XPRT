86a4db63dd100383da49d0f94fa3119ae14daa51
Eetu Halinen <eetuhalinen@Eetus-Macbook-Pro.local>
2017-02-07 14:41:44 +0200

    Stepper added to dialog/modal for testing

--- src/modules/Home/HomeView.jsx
+++ src/modules/Home/HomeView.jsx
@@ -9,6 +9,13 @@ import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
 import theme from '../../utils/theme';
 import HomeCard from '../../components/HomeCard';
+import {
+  Step,
+  Stepper,
+  StepButton,
+} from 'material-ui/Stepper';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 const styles = {
   wrapper: {
@@ -83,6 +90,36 @@ const styles = {
 
 class Home extends Component {
   state = {
+  stepIndex: 0,
+};
+
+handleNext = () => {
+  const {stepIndex} = this.state;
+  if (stepIndex < 2) {
+    this.setState({stepIndex: stepIndex + 1});
+  }
+};
+
+handlePrev = () => {
+  const {stepIndex} = this.state;
+  if (stepIndex > 0) {
+    this.setState({stepIndex: stepIndex - 1});
+  }
+};
+
+getStepContent(stepIndex) {
+  switch (stepIndex) {
+    case 0:
+      return 'Select campaign settings...';
+    case 1:
+      return 'What is an ad group anyways?';
+    case 2:
+      return 'This is the bit I really care about!';
+    default:
+      return 'You\'re a long way from home sonny jim!';
+  }
+}
+  state = {
     open: false,
   };
 
@@ -95,6 +132,9 @@ class Home extends Component {
   };
 
   render() {
+    const {stepIndex} = this.state;
+const contentStyle = {margin: '0 16px'};
+
     const actions = [
       <FlatButton
         label="Cancel"
@@ -168,7 +208,42 @@ class Home extends Component {
           modal={true}
           open={this.state.open}
         >
-          <div><p>Vat</p></div>
+          <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
+        <Stepper linear={false} activeStep={stepIndex}>
+          <Step>
+            <StepButton onClick={() => this.setState({stepIndex: 0})}>
+              Select campaign settings
+            </StepButton>
+          </Step>
+          <Step>
+            <StepButton onClick={() => this.setState({stepIndex: 1})}>
+              Create an ad group
+            </StepButton>
+          </Step>
+          <Step>
+            <StepButton onClick={() => this.setState({stepIndex: 2})}>
+              Create an ad
+            </StepButton>
+          </Step>
+        </Stepper>
+        <div style={contentStyle}>
+          <p>{this.getStepContent(stepIndex)}</p>
+          <div style={{marginTop: 12}}>
+            <FlatButton
+              label="Back"
+              disabled={stepIndex === 0}
+              onTouchTap={this.handlePrev}
+              style={{marginRight: 12}}
+            />
+            <RaisedButton
+              label="Next"
+              disabled={stepIndex === 2}
+              primary={true}
+              onTouchTap={this.handleNext}
+            />
+          </div>
+        </div>
+      </div>
         </Dialog>
       </div>
     );
