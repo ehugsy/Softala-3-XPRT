7c260c73bbd43cd59ec042cf877a80a999a6fcda
Juha Ahola <ahola.juha@hotmail.com>
2017-02-14 11:34:49 +0200

    copied styles from HomeView.jsx

--- src/modules/About/AboutView.jsx
+++ src/modules/About/AboutView.jsx
@@ -5,61 +5,91 @@ import {
   CardTitle,
   CardMedia,
 } from 'material-ui/Card';
-
-import theme from '../../utils/theme';
 import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+import theme from '../../utils/theme';
 import HomeCard from '../../components/HomeCard';
+import LoginModal from '../../components/LoginModal';
+import RegisterModal from '../../components/RegisterModal';
+
+import RaisedButton from 'material-ui/RaisedButton';
+import XprtLogo from '../../components/XprtLogo';
+import HundredLogo from '../../components/HundredLogo';
+import FuturiceLogo from '../../components/FuturiceLogo';
+
+const marginRight = '10em'
 
 const styles = {
+
   wrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start',
-    marginTop:'10%'
+    color: theme.palette.textColor,
   },
-  innerWrapper: {
+  teacherExpertWrapper: {
     display: 'flex',
     flexDirection: 'row',
-    alignItems: 'flex-end'
+    flexWrap: 'wrap',
+  },
+  secondWrapper: {
+    height: '75vmin',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  secondInnerWrapper: {
+    background: '#cccccc',
+    height: '70vmin',
+  },
+  headerWrapper:{
+    display:'flex',
+  },
+  landingWrapper:{
+    minHeight: '100vh',
+    display:'flex',
+    flexDirection: 'column',
+    justifyContent:'center',
+    paddingLeft:'4%',
+    paddingRight:'4%',
+    marginTop:'5%'
+  },
+  secondLogos:{
+    display:'flex',
+    alignSelf:'flex-end',
+    marginTop: '5em',
   },
   smallHeader: {
     color: theme.palette.primary1Color,
   },
-  cardTop: {
-    display: 'block',
-    alignSelf: 'flex-end',
+  empty:{
+    flex: 1,
+    flexShrink: 0
+  },
+  bigText: {
+    flex: 1,
     width: '50%',
-    border: 'none',
-    background: 'none',
-    boxShadow: 'none',
     fontSize: '18px',
     fontWeight: '300',
-    marginBottom: '5%'
+    minWidth: '300px',
+    maxWidth: '50%',
+    flexGrow: 1,
+    flexBasis: 'auto'
   },
-  cardLeft: {
-    border: 'none',
-    backgroundColor: theme.palette.transparentColor,
-    boxShadow: 'none',
+  leftText: {
     fontSize: '18px',
     fontWeight: '300',
     textAlign: 'right',
-    width: '40%',
-    marginLeft: '10%',
-    height: '12.5em',
+    flex:5,
+    flexBasis: '200px'
   },
-  cardRight: {
-    border: 'none',
-    backgroundColor: theme.palette.transparentColor,
-    boxShadow: 'none',
+  rightText: {
     fontSize: '18px',
     fontWeight: '300',
     borderLeft: '1px solid #333333',
     textAlign: 'left',
-    width: '40%',
-    height: '12.5em',
-    marginLeft: '3em',
-    paddingLeft: '3em'
+    flex:4,
+    marginLeft: '2em',
+    paddingLeft: '2em',
+    flexBasis: '200px'
+
+
   },
   buttonStyle: {
     border: '1px solid #555555',
@@ -79,35 +109,44 @@ class About extends Component {
 
     return(
       <div style={styles.wrapper}>
-        <Card style={styles.cardTop}>
-          <h1>About</h1>
-        </Card>
-        <div style={styles.innerWrapper}>
-          <Card style={styles.cardLeft}>
+      <div style={styles.landingWrapper}>
+        <div style={styles.headerWrapper}>
+          <div style={styles.empty}></div>
+          <div style={styles.bigText}>
+            <h1 style={styles.smallHeader}>About</h1>
+          </div>
+        </div>
+
+        <div style={styles.teacherExpertWrapper}>
+          <div style={styles.leftText}>
             <p style={styles.smallHeader}>TEACHERS</p>
-            <p>
-              Download and install the app to browse and view the experts profiles. Easily invite experts to your classroom.
-            </p>
+            <p>Download and install the app to browse and view the experts profile. Easily inite experts to your classroom.</p>
             <FlatButton label="DOWNLOAD FOR IPHONE" style={styles.buttonStyle}/><br />
             <FlatButton label="DOWNLOAD FOR ANDROID" style={styles.buttonStyle}/>
+
             <p style={styles.smallHeader}>EXPERTS</p>
+            <p>Sign up as an expert and  to share your skills for the benefit of the future generation.</p>
+            <FlatButton label="CREATE AN ACCOUNT" style={{...styles.buttonStyle, ...styles.buttonGold}} onTouchTap={this.handleOpenRegister}/><br />
+            <FlatButton label="LOGIN" style={{...styles.buttonStyle, ...styles.buttonGold}} onTouchTap={this.handleOpenLogin}/>
+            </div>
+            <div style={styles.rightText}>
               <p>
-                Sign up as an expert and to share your skills for the benefit of the future generation
-              </p>
-              <FlatButton label="CREATE AN ACCOUNT" style={{...styles.buttonStyle, ...styles.buttonGold}} onTouchTap={this.handleOpen}/><br />
-              <FlatButton label="LOGIN" style={{...styles.buttonStyle, ...styles.buttonGold}}/>
-          </Card>
-          <Card style={styles.cardRight}>
-            <p>
               Xprt connects teachers and experts for the benefit on Finnish school children. Arranging a visiting lecture from a professional expert has become easier
             </p>
             <p>
               See Xprt in action.
             </p>
             <iframe width="100%" height="315" src="https://www.youtube.com/embed/JGwWNGJdvx8" frameborder="0" allowfullscreen></iframe>
-          </Card>
+
+              </div>
+        </div>
+
+        <div style={styles.secondLogos}>
+          <HundredLogo/>
+          <FuturiceLogo/>
         </div>
       </div>
+    </div>
     );
   }
 }
