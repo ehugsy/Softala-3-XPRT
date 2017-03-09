73f0050395219dbfe238ba6b832aaba88251d315
Malkkis <jaakko.malkamaki@gmail.com>
2017-03-09 14:47:49 +0200

    Added popups for editing in MyProfile

---
+++ src/components/MyProfileEditModalCompany/index.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import Radium from 'radium';
+import theme from '../../utils/theme';
+import Checkbox from 'material-ui/Checkbox';
+
+const styles = {
+  link: {
+    ':hover': {
+      cursor:'pointer'
+    }
+  },
+  noborder: {
+    border: 0,
+  },
+  //Styling for editing Pen on left column
+  editPen: {
+    float: 'right',
+    width: '24px',
+    height: '24px'
+  },
+
+  transparentInput: {
+    backgroundColor: theme.palette.transparentColor,
+    border: '1px solid #555555',
+    marginBottom: 10,
+    color: theme.palette.textColor,
+    width: '100%',
+    maxWidth: 700,
+    padding: '10px 0px 10px 10px',
+    borderRadius: 5,
+    fontFamily: ('Catamaran' : 'sans-serif'),
+    fontSize: '16px',
+    fontWeight: '300',
+    '@media (max-width: 768px)': {
+      maxWidth: 768,
+      width: '100%',
+      borderRadius: 0
+    },
+  },
+  mobileHide: {
+    '@media (max-width: 768px)':{
+      display: 'none'
+    }
+  },
+  contactForm: {
+    paddingRight: 10
+  },
+  mainDivTextTitle: {
+    color: theme.palette.primary1Color,
+    fontSize: '16px',
+    fontWeight: '300'
+  },
+
+}
+
+@Radium
+export default class PrivacyModal extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Save"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
+    return (
+      <div>
+        <a style={styles.link} label="Dialog" onTouchTap={this.handleOpen}><img src={'../../img/edit.png'} style={styles.editPen}/></a>
+        <Dialog
+          actions={actions}
+          modal={false}
+          autoScrollBodyContent={true}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+          titleStyle={styles.noborder}
+          actionsContainerStyle={styles.noborder}
+        >
+          <div style={styles.contentCard}>
+            <form onSubmit={this.handleSubmit}style={styles.contactForm}>
+              <p style={styles.mainDivTextTitle}>COMPANY NAME:</p>
+              <input style={styles.transparentInput} type="text" placeholder="Company name" />
+              <p style={styles.mainDivTextTitle}>JOB TITLE:</p>
+              <input style={styles.transparentInput} type="text" placeholder="Job title"/>
+              <Checkbox label="Office visit possible" style={styles.checkbox}/>
+              <p>Check this box if you agree that teachers can come to your office with
+                  a group of students
+              </p>
+              <br />
+            </form>
+          </div>
+
+        </Dialog>
+      </div>
+    );
+  }
+}
