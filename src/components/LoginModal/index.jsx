dc43448cd2fed34b98d5238a39d137e6338978a1
Marko Nenonen <marko.nenonen@myy.haaga-helia.fi>
2017-03-30 14:15:04 +0300

    fixed broken loginmodal

--- src/components/LoginModal/index.jsx
+++ src/components/LoginModal/index.jsx
@@ -2,6 +2,34 @@ import React, { Component, PropTypes } from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import MyTextField from '../MyTextField';
+import theme from '../../utils/theme';
+
+const styles = {
+  buttonStyle: {
+    border: '1px solid #555555',
+    margin: 5,
+    padding: '15px',
+    borderRadius: '20px',
+    lineHeight: '0.4em',
+    marginTop: '1em'
+
+  },
+  LoginButtonStyle: {
+    border: '1px solid #555555',
+    margin: 5,
+    padding: '15px',
+    borderRadius: '20px',
+    lineHeight: '0.4em',
+  },
+  buttonGold: {
+    color: theme.palette.primary2Color,
+  },
+  dialog: {
+    width: "100%",
+    maxWidth: "350",
+    textAlign: 'center'
+  }
+}
 
 class LoginModal extends Component {
 
