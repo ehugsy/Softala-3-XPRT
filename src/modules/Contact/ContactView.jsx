e1e8802edce80f0e849c3eb28d63cb89c90a12b5
Marko Nenonen <marko.nenonen@myy.haaga-helia.fi>
2017-04-04 10:06:05 +0300

    moved ContactView styles to contactStyles.jsx

--- src/modules/Contact/ContactView.jsx
+++ src/modules/Contact/ContactView.jsx
@@ -8,7 +8,6 @@ import {
 } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
-import theme from '../../utils/theme';
 import HomeCard from '../../components/HomeCard';
 import LoginModal from '../../components/LoginModal';
 import RegisterModal from '../../components/RegisterModal';
@@ -20,148 +19,10 @@ import HundredLogo from '../../components/HundredLogo';
 import FacebookIcon from '../../components/FacebookIcon';
 import TwitterIcon from '../../components/TwitterIcon';
 import Footer from '../Footer';
+import styles from './contactStyles';
 
 const marginRight = '10em'
 
-const styles = {
-
-  wrapper: {
-    color: theme.palette.textColor,
-  },
-  teacherExpertWrapper: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    '@media (max-width: 768px)': {
-      flexDirection: 'column-reverse'
-    },
-  },
-  headerWrapper:{
-    display:'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  landingWrapper:{
-    minHeight: '80vh',
-    paddingLeft:'4%',
-    paddingRight:'4%',
-    marginTop:'20vh',
-    '@media (max-width: 768px)': {
-      marginTop: 10
-    },
-  },
-  smallHeader: {
-    color: theme.palette.primary1Color,
-    letterSpacing: '4px'
-  },
-  bigHeader: {
-    color: theme.palette.primary1Color,
-    fontWeight: 300,
-    '@media (max-width: 768px)': {
-          fontSize: 24,
-    },
-  },
-  empty:{
-    flex: 3,
-    flexBasis: '200px',
-    '@media (max-width: 768px)': {
-      display: 'none'
-    },
-  },
-  bigText: {
-    flex: 4,
-    fontSize: '18px',
-    fontWeight: '300',
-    flexBasis: '200px',
-    marginLeft: '3em',
-    paddingLeft: '3em',
-    '@media (max-width: 768px)': {
-      margin: 0,
-      padding: 0,
-      textAlign: 'center'
-    },
-  },
-  leftSpace: {
-    flex: 3,
-    '@media (max-width: 768px)': {
-      display: 'none'
-    },
-  },
-  rightSpace: {
-    flex:3,
-    '@media (max-width: 768px)': {
-      display: 'none'
-    },
-  },
-  leftText: {
-    fontSize: '18px',
-    fontWeight: '300',
-    textAlign: 'right',
-    flex:3,
-    flexBasis: '200px',
-    '@media (max-width: 768px)':{
-      textAlign: 'center',
-      paddingTop: 30,
-      fontSize: '16px',
-    },
-  },
-  rightText: {
-    fontSize: '18px',
-    fontWeight: '300',
-    borderLeft: '1px solid #333333',
-    textAlign: 'left',
-    flex:4,
-    marginLeft: '3em',
-    paddingLeft: '3em',
-    flexBasis: '200px',
-    '@media (max-width: 768px)':{
-      textAlign: 'center',
-      margin: 0,
-      padding: 0,
-      borderLeft: 0,
-      borderBottom: '1px solid rgba(204, 204, 204, .34)',
-      paddingBottom: 60,
-      fontSize: '16px',
-    },
-  },
-  buttonStyle: {
-    border: '1px solid #555555',
-    padding: '15px',
-    borderRadius: '20px',
-    lineHeight: '0.4em',
-    marginTop: '1em',
-    color: theme.palette.primary2Color,
-  },
-  transparentInput: {
-    backgroundColor: theme.palette.transparentColor,
-    border: '1px solid #555555',
-    marginBottom: 10,
-    color: theme.palette.textColor,
-    fontSize: 16,
-    width: '100%',
-    maxWidth: 350,
-    padding: '10px 0px 10px 10px',
-    borderRadius: 5,
-    fontFamily: ('Catamaran' : 'sans-serif'),
-    fontSize: 16,
-    '@media (max-width: 768px)': {
-      maxWidth: 768,
-      width: '100%',
-      borderRadius: 0
-    },
-  },
-  requiredText: {
-    color: '#999999'
-  },
-  mobileHide: {
-    '@media (max-width: 768px)':{
-      display: 'none'
-    }
-  },
-  contactForm: {
-    paddingRight: 10
-  }
-};
 @Radium
 class Contact extends Component {
   render() {
