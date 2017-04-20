de9dd5c6b345010cf737f97b5bd0976bb8c2821c
Pasi Pesonen <pesonen.p@gmail.com>
2017-04-20 10:18:44 +0300

    Added missing company field to createAccount

--- src/modules/Home/index.jsx
+++ src/modules/Home/index.jsx
@@ -14,8 +14,8 @@ export default connect(
     },
     doRegister(user) {
       dispatch(rest.actions.register.post({}, { body: JSON.stringify(
-        {name: user.name, email: user.email, password: user.password, description: user.shortIntroduction,
-          title: user.title, address: user.officeAddress, phone: user.phone, locale: 'fi', subjects: user.subjects,
+        {name: user.name, email: user.email, password: user.password, description: user.shortIntroduction, isExpert: true,
+          title: user.title, address: user.officeAddress, phone: user.phone, company: user.companyName, locale: 'fi', subjects: user.subjects,
         area: user.supportedLocations})
          }));
     } // TODO: Missing fields CompanyName
